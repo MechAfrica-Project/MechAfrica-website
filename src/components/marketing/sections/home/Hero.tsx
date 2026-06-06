ece7d3d8328b87859c018e8638fb356292e010e5
elvisowusu <elvisgyasiowusu24@gmail.com>
2026-06-06 23:03:31 +0000

    fix: adjust layout and styling in HomeHero for improved responsiveness

--- src/components/marketing/sections/home/Hero.tsx
+++ src/components/marketing/sections/home/Hero.tsx
@@ -16,7 +16,7 @@ export function HomeHero() {
       </div>
 
       <div
-        className="absolute inset-x-0 top-[101px] h-[105px] bg-white md:top-[165px] md:h-[300px] lg:top-[215px] lg:h-[340px] 2xl:top-[245px] 2xl:h-[390px]"
+        className="absolute inset-x-0 top-[101px] h-[105px] bg-white md:h-[300px]  lg:h-[340px] 2xl:h-[390px]"
         style={{ clipPath: "polygon(0 24%, 100% 0, 100% 100%, 0 100%)" }}
       />
 
@@ -32,14 +32,14 @@ export function HomeHero() {
           width={790}
           height={659}
           priority
-          className="absolute left-[5px] top-[100px] z-10 w-55"
+          className="absolute left-[5px] top-[100px] md:top-27 lg:top-22 z-10 w-55 md:w-95 lg:w-140"
         />
 
-        <div className="absolute right-[55px] top-[115px] z-20 max-w-[205px] text-primary max-[480px]:right-[18px] max-[480px]:top-[130px] max-[480px]:max-w-[145px] max-[360px]:right-[14px] max-[360px]:max-w-[130px] md:right-[8%] md:top-[210px] md:max-w-[340px] lg:right-[11%] lg:top-[275px] lg:max-w-[390px] xl:right-[12.5%]">
+        <div className="absolute right-[55px] top-[115px] z-20 max-w-[205px] text-primary max-[480px]:right-[18px] max-[480px]:top-[130px] max-[480px]:max-w-[145px] max-[360px]:right-[14px] max-[360px]:max-w-[130px] md:right-[8%] md:top-[170px] md:max-w-[340px] lg:right-[11%] lg:top-[180px] lg:max-w-[390px] xl:right-[12.5%]">
           <h1 className="font-serif text-[20px] font-light leading-tight tracking-normal max-[480px]:text-[18px] md:text-[34px] lg:text-[40px] xl:text-[42px]">
             <strong className="font-extrabold">Mech</strong> Africa
           </h1>
-          <p className="mt-2 text-[10.5px] font-medium leading-[1.45] text-primary max-[480px]:text-[9.5px] md:mt-4 md:text-[16px] lg:text-[18px] xl:text-[19px]">
+          <p className="mt-2 text-[10.5px] font-medium leading-[1.45] text-primary max-[480px]:text-[9.5px] md:mt-4 md:text-[16px] lg:text-[18px] xl:text-[22px]">
             MechAfrica connects farmers struggling to find timely, reliable
             services to Providers who struggle with visibility and coordination
             — via <strong className="font-bold">mobile apps</strong> and{" "}
